b39b02447a34d9da340de97614e42bfc971ed533
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-02-19 12:05:42 +0300

    Initial commit

---
+++ src/main/java/team04/depoYonetim/Runner.java
@@ -0,0 +1,13 @@
+package team04.depoYonetim;
+
+
+
+public class Runner {
+    public static void main(String[] args) {
+        Depo.urun();
+        Depo.menu();
+    }
+
+    public static void menu() {
+    }
+}
