11bdcebb11621851c88953db68cc496a5e8ce932
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-02-19 12:05:42 +0300

    Initial commit200223

---
+++ src/main/java/mini_projects/MiniBookStore/ProductService.java
@@ -0,0 +1,9 @@
+package mini_projects.MiniBookStore;
+//2-product işlemleri için standart
+public interface ProductService {
+    void processMenu();
+    void listProduct();
+    void  addProduct();
+    void deleteProduct();
+    void filterProducts(String filter);
+}
